84cffd0913de0e32575fd9f2f5959008766bdb04
Husnain <husnainhaider1218@gmail.com>
2025-11-23 15:44:16 +0000

    database utility functions including connection check

--- Back End/database.py
+++ Back End/database.py
@@ -14,3 +14,15 @@ def get_db():
     conn.row_factory = sqlite3.Row
     return conn
 
+def check_connection():
+    """
+    Checks if the database can be connected successfully.
+    Returns True if connection is OK.
+    """
+    try:
+        conn = get_db()
+        conn.execute("SELECT 1")
+        conn.close()
+        return True
+    except Exception:
+        return False
